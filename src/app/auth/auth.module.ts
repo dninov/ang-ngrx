dfd1ba3462d5fb1481a32478275d6489472710e9
Delyan Ninov <dninov@gmail.com>
2022-01-15 16:45:11 +0200

    IC

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,19 @@
-import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
-import {RegisterComponent} from './components/register/register.component'
+import {CommonModule} from '@angular/common'
 
-const routes = [{path: 'register', component: RegisterComponent}]
+import {RegisterComponent} from 'src/app/auth/components/register/register.component'
+import {RouterModule} from '@angular/router'
+import {ReactiveFormsModule} from '@angular/forms'
+
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+]
+
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
-  declarations: [RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  declarations: [RegisterComponent]
 })
 export class AuthModule {}
